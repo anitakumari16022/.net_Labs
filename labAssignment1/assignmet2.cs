912e4472eeee98e518db3077e2ce40c7d458ef87
Anita Kumari <12yashika1234@gmail.com>
2025-07-31 22:54:16 +0530

    second try

---
+++ labAssignment1/assignmet2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labAssignment1
+{
+    internal class assignmet2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("second Assignment);
+                }
+    }
+}
