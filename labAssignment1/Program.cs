0b383f5a97431db1a6fcd0bbe95ea3c005239121
Anita Kumari <12yashika1234@gmail.com>
2025-07-31 22:28:55 +0530

    first lab

---
+++ labAssignment1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labAssignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] trascations = { 200, -150, 340, 500, -100 };
+            int sum = 0;
+            for (int i = 0; i < trascations.Length; i++)
+            {
+              
+                sum += trascations[i];
+                
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
